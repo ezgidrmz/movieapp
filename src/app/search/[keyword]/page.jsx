3e1ece21e0e3b5e6076b875feac25d15610d3f66
ezgidrmz <ezgidurmaz999@gmail.com>
2024-07-03 15:40:17 +0300

    page2

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,18 +1,12 @@
 import Movies from '@/components/Movies';
-import React, { Suspense } from 'react'
+import React from 'react'
 
-export async function getServerSideProps(context) {
-    const { keyword } = context.params;
+const Page = async ({ params }) => {
+    const keyword = params.keyword;
 
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=b3573e939842f9b8e715b292b9500933&query=${keyword}&language=en-US&include_adult=false`)
     const data = await res.json();
 
-    return {
-        props: { data },
-    };
-}
-
-const Page = ({ data }) => {
     console.log(data?.results, "data")
 
     return (
@@ -32,10 +26,4 @@ const Page = ({ data }) => {
     )
 }
 
-export default function WrappedPage(props) {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Page {...props} />
-        </Suspense>
-    );
-}
+export default Page
